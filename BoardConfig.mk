c051ea7d66230eee147f5a66f37d7553143bff2a
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2015-02-09 20:10:42 +0900

    Set TARGET_USERIMAGES_USE_EXT4.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ ENABLE_LOKI_RECOVERY := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_FSTAB := device/kyocera/201k/root/fstab.qcom
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/kyocera/201k/recovery/keys.c
+TARGET_USERIMAGES_USE_EXT4 := true
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 no_console_suspend=1 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2
 BOARD_KERNEL_BASE := 0x80200000
